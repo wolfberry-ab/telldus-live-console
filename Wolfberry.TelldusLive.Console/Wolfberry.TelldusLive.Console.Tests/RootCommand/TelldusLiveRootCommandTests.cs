1da6f57bc007a404acf624348072385d3314765e
Mikael Johansson <mikael.johansson@wolfberry.se>
2025-11-18 20:39:00 +0100

    chore: update nugets

--- Wolfberry.TelldusLive.Console/Wolfberry.TelldusLive.Console.Tests/RootCommand/TelldusLiveRootCommandTests.cs
+++ Wolfberry.TelldusLive.Console/Wolfberry.TelldusLive.Console.Tests/RootCommand/TelldusLiveRootCommandTests.cs
@@ -3,21 +3,20 @@ using Xunit;
 using Moq;
 using Wolfberry.TelldusLive.Console.Configuration;
 
-namespace Wolfberry.TelldusLive.Console.Tests.RootCommand
+namespace Wolfberry.TelldusLive.Console.Tests.RootCommand;
+
+public class TelldusLiveRootCommandTests
 {
-    public class TelldusLiveRootCommandTests
+    [Fact]
+    public void TestCreate_ReturnsCommand()
     {
-        [Fact]
-        public void TestCreate_ReturnsCommand()
-        {
-            var mockedConfiguration = new AuthConfiguration();
-            var configurationManager = new Mock<IConfigurationManager>();
-            configurationManager.Setup(x => x.GetAuthConfiguration())
-                .Returns(mockedConfiguration);
-            
-            var command = TelldusLiveRootCommand.Create(configurationManager.Object);
-            
-            Assert.NotNull(command);
-        }
+        var mockedConfiguration = new AuthConfiguration();
+        var configurationManager = new Mock<IConfigurationManager>();
+        configurationManager.Setup(x => x.GetAuthConfiguration())
+            .Returns(mockedConfiguration);
+        
+        var command = TelldusLiveRootCommand.Create(configurationManager.Object);
+        
+        Assert.NotNull(command);
     }
 }
